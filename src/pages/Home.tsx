8f9cf75a33194936dc7e0d4456c60d99d1acaddb
eobcre <eobcre@gmail.com>
2023-09-20 13:37:24 -0400

    updated style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,45 +3,46 @@ import ResImage_1 from '../assets/image/geo_1.png';
 import ResImage_2 from '../assets/image/geo_2.png';
 import ResImage_3 from '../assets/image/geo_3.png';
 import ResImage_4 from '../assets/image/geo_4.png';
+import ModelImage from '../assets/image/model.jpg';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <div className='flex justify-center items-center h-screen'>
-        <div className='relative font-main text-4xl leading-normal w-[50%]'>
-          <p className='text-orange text-2xl'>Hey,</p>
-          <p className='text-blue'>Your hair is our canvas!</p>
-          <p className='text-pink'>
-            Beautiful hair with beautiful care
-            <span className='text-red text-5xl ml-2'>.</span>
-          </p>
+    <div className='relative'>
+      <div className='flex justify-center h-[100dvh] md:h-screen'>
+        <div className='font-main text-4xl leading-normal w-[50%]'>
+          <img src={ModelImage} alt='Image' />
+          <div className='pt-8 min-[500px]:pt-6 min-[1100px]:pt-2'>
+            <p className='text-orange text-2xl'>Hey,</p>
+            <p className='text-blue'>Your hair is our canvas!</p>
+            <p className='text-pink'>
+              Beautiful hair with beautiful care
+              <span className='text-red text-5xl ml-2'>.</span>
+            </p>
+          </div>
         </div>
-        <img
-          src={MainImage}
-          alt='Image'
-          className='max-h-screen max-[780px]:hidden'
-        />
+        <img src={MainImage} alt='Image' className='h-screen hidden md:block' />
       </div>
-      <div className='min-[781px]:hidden'>
+
+      <div className='md:hidden'>
         <img
           src={ResImage_1}
           alt='Image_1'
-          className='absolute top-[60px] min-[400px]:w-[100px] max-[500px]:w-[130px]'
+          className='absolute top-0 w-[80px]'
         />
         <img
           src={ResImage_2}
           alt='Image_2'
-          className='absolute top-[60px] right-0 min-[400px]:w-[100px] max-[500px]:w-[130px]'
+          className='absolute top-0 right-0 w-[80px]'
         />
         <img
           src={ResImage_3}
           alt='Image_3'
-          className='absolute bottom-[-60px] min-[400px]:w-[100px] max-[500px]:w-[130px]'
+          className='absolute bottom-0 w-[80px]'
         />
         <img
           src={ResImage_4}
           alt='Image_4'
-          className='absolute bottom-[-60px] right-0 min-[400px]:w-[100px] max-[500px]:w-[130px]'
+          className='absolute bottom-0 right-0 w-[80px]'
         />
       </div>
     </div>
