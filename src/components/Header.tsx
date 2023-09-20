b8e2ffe30c709c2b2294fb66097c57b62bd742da
eobcre <eobcre@gmail.com>
2023-09-19 23:45:11 -0400

    nav styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,14 @@ import { navData } from '../data/navData';
 
 const Header: React.FC = () => {
   return (
-    <header>
+    <header className='bg-[#5173B4]'>
       <nav>
-        <ul>
+        <ul className='flex justify-center'>
           {navData.map((nav) => (
-            <li id={nav.id}>
+            <li
+              id={nav.id}
+              className='font-nav text-[#EDE6D4] text-xl p-4 hover:text-[#E58D05]'
+            >
               <a href={nav.link}>{nav.name}</a>
             </li>
           ))}
