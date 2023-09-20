1232b04e91e9ef3e894972697d37dc9770db5e30
eobcre <eobcre@gmail.com>
2023-09-19 23:26:48 -0400

    placed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+import Salon from './pages/Salon';
+import Service from './pages/Service';
+import Gallery from './pages/Gallery';
+import Book from './pages/Book';
+import Footer from './components/Footer';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/salon' element={<Salon />} />
+        <Route path='/service' element={<Service />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/book' element={<Book />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
