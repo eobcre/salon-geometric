299a1a88aa4d9fc59fbf4e325dbfdd0540bd1533
eobcre <eobcre@gmail.com>
2023-09-20 00:48:08 -0400

    updated custom color class names

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import { navData } from '../data/navData';
 
 const Header: React.FC = () => {
   return (
-    <header className='bg-[#5173B4]'>
+    <header className='bg-blue'>
       <nav>
         <ul className='flex justify-center'>
           {navData.map((nav) => (
             <li
               id={nav.id}
-              className='font-nav text-[#EDE6D4] text-xl p-4 hover:text-[#E58D05]'
+              className='font-main text-ivory text-xl p-4 hover:text-orange'
             >
               <a href={nav.link}>{nav.name}</a>
             </li>
