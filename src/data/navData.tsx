57f378f80777f9926cead7a5787919fcd92c9bcb
eobcre <eobcre@gmail.com>
2023-09-19 23:25:43 -0400

    created data for nav

---
+++ src/data/navData.tsx
@@ -0,0 +1,29 @@
+import { nanoid } from 'nanoid';
+
+export const navData = [
+  {
+    id: nanoid(),
+    name: 'Home',
+    link: './',
+  },
+  {
+    id: nanoid(),
+    name: 'Salon',
+    link: './salon',
+  },
+  {
+    id: nanoid(),
+    name: 'Service',
+    link: './service',
+  },
+  {
+    id: nanoid(),
+    name: 'Gallery',
+    link: './gallery',
+  },
+  {
+    id: nanoid(),
+    name: 'Booking',
+    link: './book',
+  },
+];
